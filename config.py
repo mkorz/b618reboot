f3f35d33f59f91c97cfd230b4f9e939dd1b09520
Emanuele Cipolla <emanuele@emanuelecipolla.net>
2021-02-25 22:13:28 +0100

    Scripts to reboot router if no connection

--- config.py
+++ config.py
@@ -2,3 +2,7 @@
 ROUTER = "192.168.1.1"
 USER = "admin"
 PASSWORD = b"admin-password"
+TEST_HOST = '8.8.8.8'
+TEST_PORT = 53
+TEST_TIMEOUT = 3
+CHECK_TIMEOUT = 60
---
+++ reboot_router_if_no_connection_background.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+./reboot_router_if_no_connection.py >/dev/null 2>&1 &
+
